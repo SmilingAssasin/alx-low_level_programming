4b5603ee7404377a962cfb0b71f0fbd8e0d8ccff
Calvin Oirere <oirerecalvin@gmail.com>
2022-06-08 12:35:19 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,12 @@
 /**
  * print_alphabet - print alphabet lowercase
  * followed by a new line
- * Return: Always 0
  */
 void print_alphabet(void)
 {
 	int i;
 	
 	for (i = 0, i = 26, i++)
-	{
 		_putchar(i);
-	}
 	_putchar('\n');
 }
