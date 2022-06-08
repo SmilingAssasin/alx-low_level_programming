8b0c708ee84ee50192cabee0bcbb05036d8ac67a
Calvin Oirere <oirerecalvin@gmail.com>
2022-06-08 16:18:57 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0, i <24; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0, j < 60; j++)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar(i / 10 + 48);
 			_putchar(i % 10 + 48);
