06cf2f2b03a75b28385c21404ff0a2cf0b16e7d3
Calvin Oirere <oirerecalvin@gmail.com>
2022-06-08 17:21:49 -0700

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - print numbers
  * @n: input integer
- * prints all natural numbers from n to 98 
+ * prints all natural numbers from n to 98
  * followed by a new line
  * Return: void
  */
